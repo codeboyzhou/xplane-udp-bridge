056fbef581b9a5e5c4bd15cf0e0456820eaaee37
codeboyzhou <imzhouchen@gmail.com>
2026-01-24 16:52:36 +0800

    chore: remove flutter project files and resources

--- client/flutters/android/app/src/main/kotlin/com/example/flutters/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.flutters
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- client/flutters/lib/helper.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:logger/logger.dart';
-
-final Logger logger = Logger(printer: SimplePrinter(colors: false));
-
-void showSnackBar(BuildContext context, String message) {
-  ScaffoldMessenger.of(context).hideCurrentSnackBar();
-  ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(message)));
-}
